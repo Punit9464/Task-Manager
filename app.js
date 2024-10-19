ab1d0b70713051d738191908f9b7061f3619dad9
Punit9464 <punitkumar8728@gmail.com>
2024-10-19 17:00:24 +0530

    navbar make

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const port = 3000;
 const path = require("path");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static(path.join(__dirname + "/public")));
+app.set("views", path.join(__dirname + "/views"));
 app.use(express.urlencoded({ extended: true }));
-app.set("views", path.resolve(__dirname, "views"));
 
 app.get("/", (req,res) => {
-    res.render("home.ejs");
+    res.render("tasks/index.ejs");
 });
 
 app.listen(port, () => {
-    console.log("Server is Listening to the Port:", port);
+    console.log("Server is Listening to the Port:", port + "\nhttp://localhost:"+port);
 });
